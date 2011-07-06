53fadeb7ce54d16c05442344e54d26f3a30642fd
Jannis Grimm <jannis@gje.ch>
2011-07-06 18:44:39 +0000

    Twig-Test

--- app.php
+++ app.php
@@ -1,13 +1,22 @@
 <?php
 require_once __DIR__.'/vendor/silex/silex.phar';
-
 $app = new Silex\Application();
 
-$app->get('/', function() {
+// Konfigurieren
+$app->register(new Silex\Extension\TwigExtension(), array(
+    'twig.path'		  => __DIR__.'/views',
+    'twig.class_path' => __DIR__.'/vendor/twig'
+));
+
+$app->get('/', function () {
     return "Huffle-Newsletter!";
 });
 
-$app->get('/twigtest', function() {
+$app->get('/twigtest/{name}', function ($name) use ($app) {
+    return $app['twig']->render('test.twig', array(
+        'name'  => $name,
+        'email' => $name.'@example.com'
+    ));
 });
 
 return $app;

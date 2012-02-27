fc8a7ddf9990be009f82f3b3986c8c9505adb8a9
Jannis Grimm <jannis@gje.ch>
2012-02-27 17:27:35 +0100

    Cleaning up

--- update.sql
+++
@@ -1,6 +0,0 @@
--- Update von altem Newsletter-System
-ALTER TABLE  `huffle_members` CHANGE  `uid`  `id` INT( 11 ) NOT NULL AUTO_INCREMENT;
-ALTER TABLE  `huffle_members` CHANGE  `first`  `registerdate` DATETIME NOT NULL;
-ALTER TABLE  `huffle_members` ADD UNIQUE  (`mail`);
-ALTER TABLE  `huffle_members` DROP  `last`;
-ALTER TABLE  `huffle_news` CHANGE  `nid`  `id` INT( 11 ) NOT NULL AUTO_INCREMENT;

55d1c38dd79a42674a7ec24c9ebf9162d2ce1141
Jannis Grimm <jannis@gje.ch>
2017-02-03 02:18:21 +0100

    Use utf8mb4 for MySQL

--- install.sql
+++ install.sql
@@ -1,15 +1,15 @@
-CREATE TABLE `hnews_members` (
+CREATE TABLE `huffle_members` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `mail` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `mail` varchar(50) NOT NULL,
   `registerdate` datetime NOT NULL,
   `type` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
 
-CREATE TABLE `hnews_news` (
+CREATE TABLE `huffle_news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
-  `text` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `text` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
